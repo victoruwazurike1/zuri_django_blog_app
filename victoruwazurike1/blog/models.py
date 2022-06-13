f455b151b532e5a16cac140d3c0b38145a4af328
Uwazurike Victor <victoru779@gmail.com>
2022-06-13 21:10:39 +0100

    modified models.py

--- victoruwazurike1/blog/models.py
+++ victoruwazurike1/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='blog_posts')
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now=True)
